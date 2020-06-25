fcc9dd5eb826114ada7da4e7c8ff23978c4f075c
Jermaine Bhoorasingh <jermaine@jermaine.co>
2020-06-25 09:44:01 -0400

    started working on login logic

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,36 @@
 import React, {useState} from "react";
-import {Button, FormControl, FormGroup,  Input, InputLabel } from "@material-ui/core";
+import {Button, FormControl, FormGroup, Input, InputLabel} from "@material-ui/core";
 import "./Login.css";
+import axios from 'axios';
 
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isLoggedIn, setLoggedIn] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     function validateForm() {
         return email.length > 0 && password.length > 0;
-    }
-
+    };
+ //https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/
+    //https://medium.com/better-programming/building-basic-react-authentication-e20a574d5e71
     function handleSubmit(event) {
         event.preventDefault();
-        console.log("Hello")
+        console.log(`---- Login Logic ${email} ${password}`);
+        //let data = "{\"username\":\"paul@nomail.com\",\"password\":\"password\"}";
+        //let data = `{"username: "${email}", "password": "${password}"}`;
+        let data = `{
+  "username": "${email}",
+  "password": "${password}"
+}`;
+        console.log(data);
+        axios.post('http://10.0.0.241:5000/api/v1/auth/', data)
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });
     }
 
     return (
